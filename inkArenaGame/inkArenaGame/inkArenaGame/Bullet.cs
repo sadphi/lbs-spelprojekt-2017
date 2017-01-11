fd67a854fd24caf38345195a51e98d3e4750fb31
KrissLaCross <kristoffer.franzon@learnet.se>
2017-01-11 11:52:21 +0100

    Fixed the bullet class
    again...

---
+++ inkArenaGame/inkArenaGame/inkArenaGame/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace inkArenaGame
+{
+    class Bullet
+    {
+        Vector2 position;
+        Vector2 velocity;
+
+        public Bullet(Vector2 newPos, Vector2 newVel)
+        {
+            position = newPos;
+            velocity = newVel;
+        }
+
+        public void Update()
+        {
+            position += velocity;
+        }
+
+        public void Draw(Texture2D bullet)
+        {
+            Game1.spriteBatch.Draw(bullet, position, null, Color.White, MathHelper.ToRadians((float)Math.Atan2(velocity.Y, velocity.X)), Vector2.One / 2.0f, Vector2.One, SpriteEffects.None, 0);
+        }
+    }
+}
